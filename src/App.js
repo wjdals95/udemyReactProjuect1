044a6f5777e0116d6c3872fec8bb2dfdd978ac29
wjdals95 <wjdals992@naver.com>
2022-12-23 16:17:03 +0900

    래퍼 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import Expenses from "./components/Expenses";
-import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   const expenses = [
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,12 @@
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 
 import './Expenses.css'
 
 function Expenses(props){
     
     return(
-        <div className='expenses'>
+        <Card className='expenses'>
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -26,7 +27,7 @@ function Expenses(props){
                 amount={props.items[3].amount}
                 date={props.items[3].date}
             />
-        </div>
+        </Card>
     )
 }
 
