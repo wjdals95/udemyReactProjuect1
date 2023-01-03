9b8e8156cc1763e5729018f0219a56f6bcacba26
wjdals95 <wjdals992@naver.com>
2023-01-03 11:36:37 +0900

    양방향바인딩 수정

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 
 import './ExpenseItem.css';
 import Card from "../UI/Card";
@@ -6,7 +6,6 @@ import ExpenseDate from './ExpenseDate';
 
 
 const ExpenseItem = (props) => {
-    
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
@@ -14,7 +13,6 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler} >Change Title</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,11 +3,10 @@ import React,{useState} from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
-
 import './Expenses.css'
 
 const Expenses = (props) => {
-    const[filteredYear, setFilteredYear] = useState(2020);
+    const[filteredYear, setFilteredYear] = useState('2020');
 
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear)
@@ -15,7 +14,7 @@ const Expenses = (props) => {
     return(
         <div>
             <Card className='expenses'>
-                <ExpensesFilter seleted={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
                 <ExpenseItem
                     title={props.items[0].title}
                     amount={props.items[0].amount}
